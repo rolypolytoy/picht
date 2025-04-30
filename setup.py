50510a7f337a3f0a9050a73487575228b32429c0
Attack Orange <rishiitsharma@gmail.com>
2025-04-30 22:06:08 +0400

    v0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="picht",
-    version="0.1.4",
+    version="0.1.5",
     author="Rishiit Sharma",
     author_email="rishiitsharma@gmail.com",
     description="Electron optics simulation library for electron microscopy applications",
