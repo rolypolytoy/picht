3395db176901909352b6d8f7d236b0a9b41bac45
Attack Orange <rishiitsharma@gmail.com>
2025-05-08 10:33:43 +0400

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="picht",
-    version="1.1.3",
+    version="1.1.4",
     author="Rishiit Sharma",
     author_email="rishiitsharma@gmail.com",
     description="Electron and ion optics simulation using the Finite Difference Method (FDM)",
