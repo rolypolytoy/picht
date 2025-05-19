a37a5424c6d3d2ac44435c722e07a5771640afb6
Attack Orange <rishiitsharma@gmail.com>
2025-05-19 23:45:19 +0400

    prep for 2.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="picht",
-    version="2.1.8",
+    version="2.2.0",
     author="Rishiit Sharma",
     author_email="rishiitsharma@gmail.com",
     description="Electron optics simulation tool",
